21f0e5f0ec2e354746e916383115626974f54669
tzils <30622657+tzils@users.noreply.github.com>
2017-09-22 22:44:00 +0200

    Add files via upload

--- OSinfo.js
+++ OSinfo.js
@@ -1,5 +1,5 @@
 var os = require('os');
-
+var colors = require('colors');
 var czas = require('./czas').print;
 
 function OSinfo() {
@@ -13,12 +13,22 @@ function OSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (czas(uptime)));
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System:'.grey, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: ~'.green, (czas(uptime)));
+    console.log('User name:'.yellow, userInfo.username);
+    console.log('Home dir:'.gray, userInfo.homedir);
 }
 
 exports.print = OSinfo;
+
+
+var colors = require('colors');
+ 
+console.log('hello'.green); // outputs green text 
+console.log('i like cake and pies'.underline.red) // outputs red underlined text 
+console.log('inverse the color'.inverse); // inverses the color 
+console.log('OMG Rainbows!'.rainbow); // rainbow 
+console.log('Run the trap'.trap); // Drops the bass 
+ 
