0f1edfe928ccd4fb11926a66347f3d825aea89cd
tzils <30622657+tzils@users.noreply.github.com>
2017-11-08 14:09:47 +0100

    Add files via upload

---
+++ ContactForm.js
@@ -0,0 +1,17 @@
+ var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  )
+  },
+
+  render: function() {
+    return (
+    <form className={'contactForm'}>
+      <input type='text' placeholder:'Imię' value: {this.props.contact.firstName}</input>
+      <input type='text' placeholder:'Nazwisko' value: {this.props.contact.lastName}</input>
+      <input type='email' placeholder:'Email' value: {this.props.contact.email}</input>
+      <button type='submit'>Dodaj kontakt</button>
+    </form>
+    )
+   }
+  },
